59d20e0bd2b67ef594ef1909817ecb0e1e4e6f10
MrBennySolomon <MrBennySolomon@gmail.com>
2023-02-10 13:06:30 +0200

    getting data from api ver 1.2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
-
+  const btnEurope  = document.querySelector('.europe');
+  const btnAsia    = document.querySelector('.asia');
+  const btnAmerica = document.querySelector('.america');
+  const btnAfrica  = document.querySelector('.africa');
 
   const ctx = document.getElementById('myChart');
-  const printChart = (country, city, yearArr, populationArr) => {
+  const printChart = (country, citiesArr, populationArr, yearsArr) => {
     new Chart(ctx, {
       type: 'bar',
       data: {
         
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [...citiesArr],
         datasets: [{
-          // tickcolor: 'red',
-          label: '# of Votes',
-          borderColor: ['red', 'blue', 'yellow'],
-          backgroundColor: ['blue', 'yellow', 'red'],
+          label: yearsArr[0],
+          borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)'
+        ],
+          backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+        ],
   
-          data: [12, 19, 3, 5, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-          borderWidth: 5
+          data: [...populationArr],
+          borderWidth: 2
         }]
       },
       options: {
@@ -35,16 +45,17 @@
     const info = await response.json();
 
     ctx.style.background = `url(${info[0].flags.png}) no-repeat center center/cover`;
-
+    //localStorage.setItem('countryData', info);
     return info;
   };
   
   const israel = fetchCountry('israel');
+  const france = fetchCountry('france');
 
   const fetchRegion = async (region) => {
     const response = await fetch(`https://restcountries.com/v3.1/region/${region}`);
     const info = await response.json();
-
+    //localStorage.setItem('continentsData', info);
     return info;
   };
   
@@ -53,34 +64,68 @@
   const fetchPopulation = async () => {
     const response = await fetch(`https://countriesnow.space/api/v0.1/countries/population/cities`);
     const info = await response.json();
-
-    getPopulation(info.data);
+    //localStorage.setItem('populationData', info.data);
+    //getPopulation(info.data, 'Israel');
+    getPopulation(info.data, 'France');
   };
   
   const allPopulation = fetchPopulation();
   
-  const getPopulation = async (data) => {
+  const getPopulation = async (data, country) => {
+    const citiesArr = [];
+    const yearsArr = [];
+    const populationArr = [];
     for (let i = 0; i < data.length; i++) {
-    
-      const obj   = {};
-      obj.data    = [];
+      const obj = {};
+      obj.data = [];
+      if (data[i].country === country) {
+        const citySet = new Set();
 
-      for (let j = 0; j < data[i].populationCounts.length; j++) {
-        
-        
-        if (data[i].populationCounts[j].value !== 'null' &&
-            data[i].city != Number(data[i].city)) {
-          obj.data.push(
-            {
-              city:       data[i].city,
-              year:       data[i].populationCounts[j].year,
-              population: data[i].populationCounts[j].value
-            }
-          );
+
+        for (let j = 0; j < data[i].populationCounts.length; j++) {
+          
+          
+          if (data[i].populationCounts[j].value !== 'null' &&
+              data[i].city != Number(data[i].city)) {
+                citySet.add(data[i].city);
+
+              
+            obj.data.push(
+              {
+                city:       data[i].city,
+                year:       data[i].populationCounts[j].year,
+                population: data[i].populationCounts[j].value
+              }
+            );
+          }
         }
+        citiesArr.push(data[i].city);
+        yearsArr.push(data[i].populationCounts[0].year);
+        populationArr.push(data[i].populationCounts[0].value);
+        //localStorage.setItem(`${data[i].country}-${data[i].city}`, JSON.stringify(obj));
       }
-      localStorage.setItem(`${data[i].country}-${data[i].city}`, JSON.stringify(obj));
     }
     //console.log(localStorage.getItem('israel-Ashdod'));
-    //printChart('israel', localStorage.get('israel')[0].city, localStorage.get('israel')[0].year, localStorage.get('israel')[0].population )
+    // console.log(citiesArr);
+    // console.log(yearsArr);
+    // console.log(populationArr);
+    printChart('israel', citiesArr, populationArr, yearsArr)
   }
+
+btnEurope.addEventListener('click', (event) => {
+  const europe = fetchRegion('Europe');
+  console.log('europe', europe);
+});
+btnAsia.addEventListener('click', (event) => {
+  const asia = fetchRegion('Asia');
+  console.log('asia', asia);
+
+});
+btnAmerica.addEventListener('click', (event) => {
+  const america = fetchRegion('Americas');
+  console.log('america', america);
+});
+btnAfrica.addEventListener('click', (event) => {
+  const africa = fetchRegion('Africa');
+  console.log('africa', africa);
+});
